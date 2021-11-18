d5f323d7a30d9f46975bd4cd981c83da0e01e971
WeijieProg <77503225+WeijieProg@users.noreply.github.com>
2021-11-18 18:50:56 +0800

    Update to run on CCS

--- Debug/subdir_vars.mk
+++ Debug/subdir_vars.mk
@@ -13,41 +13,47 @@ C_SRCS += \
 ../HCSR04.c \
 ../Motor_Driver.c \
 ../main.c \
-../system_msp432p401r.c 
+../system_msp432p401r.c \
+../test.c 
 
 C_DEPS += \
 ./ESP8266_UART.d \
 ./HCSR04.d \
 ./Motor_Driver.d \
 ./main.d \
-./system_msp432p401r.d 
+./system_msp432p401r.d \
+./test.d 
 
 OBJS += \
 ./ESP8266_UART.obj \
 ./HCSR04.obj \
 ./Motor_Driver.obj \
 ./main.obj \
-./system_msp432p401r.obj 
+./system_msp432p401r.obj \
+./test.obj 
 
 OBJS__QUOTED += \
 "ESP8266_UART.obj" \
 "HCSR04.obj" \
 "Motor_Driver.obj" \
 "main.obj" \
-"system_msp432p401r.obj" 
+"system_msp432p401r.obj" \
+"test.obj" 
 
 C_DEPS__QUOTED += \
 "ESP8266_UART.d" \
 "HCSR04.d" \
 "Motor_Driver.d" \
 "main.d" \
-"system_msp432p401r.d" 
+"system_msp432p401r.d" \
+"test.d" 
 
 C_SRCS__QUOTED += \
 "../ESP8266_UART.c" \
 "../HCSR04.c" \
 "../Motor_Driver.c" \
 "../main.c" \
-"../system_msp432p401r.c" 
+"../system_msp432p401r.c" \
+"../test.c" 
 
 
